c91c12af0edde671b2033c2c4e5c2715de9e0093
Snigdha Reddy Pulim <snigdhapulim2@gmail.com>
2020-04-14 07:53:59 +0530

    Updates

---
+++ precache-manifest.4969b273f955a3e9ed94c731a2b844e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8144e83783528414b3e2d6c54c941968",
+    "url": "/Bootstrap-Cards/index.html"
+  },
+  {
+    "revision": "ff2957a9ddfa9c05367f",
+    "url": "/Bootstrap-Cards/static/js/2.c607299e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Bootstrap-Cards/static/js/2.c607299e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9969ee9b91f24e7ca286",
+    "url": "/Bootstrap-Cards/static/js/main.55a1d23f.chunk.js"
+  },
+  {
+    "revision": "2d758c99f9df700827a7",
+    "url": "/Bootstrap-Cards/static/js/runtime-main.50bdaf53.js"
+  }
+]);
